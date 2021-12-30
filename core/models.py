b8c9e0fa28b2991067f11a1b6c3ad7aeb5da0e22
Lucas Sousa <lucas103101@gmail.com>
2021-12-30 19:23:59 -0300

    feat: create models message, user and add in django admin

---
+++ core/models.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.models import AbstractBaseUser
+from django.db import models
+import uuid
+
+
+class BaseModel(models.Model):
+    uuid = models.UUIDField(unique=True, default=uuid.uuid4)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class User(BaseModel, AbstractBaseUser):
+    STATUS_ONLINE = "online"
+    STATUS_OFFLINE = "online"
+
+    STATUS_CHOICE = (
+        (STATUS_ONLINE, "online"),
+        (STATUS_OFFLINE, "offline")
+    )
+    name = models.CharField("Nome", max_length=50, null=False, blank=False)
+    username = models.CharField("Username", max_length=50, unique=True)
+    image = models.ImageField("Imagem", upload_to="media/avatar", null=True, blank=True)
+    last_ping = models.DateField("Visto por último", null=True, blank=True)
+    status = models.CharField("Status", choices=STATUS_CHOICE, max_length=10, blank=True, null=True)
+
+    USERNAME_FIELD = 'username'
+
+    def __str__(self) -> str:
+        return self.username
+    
+    class Meta:
+        ordering = ["-name"]
+        verbose_name = 'Usuário'
+        verbose_name_plural = 'Usuários'
