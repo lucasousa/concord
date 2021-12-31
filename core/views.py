7430ede9346e96bf062ff163184bf050dfb7b4d3
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2021-12-31 11:40:32 -0300

    login page

--- core/views.py
+++ core/views.py
@@ -1,5 +1,16 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 
 def index(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("core:home"))
+
+    return render(request, "core/login.html")
+
+
+@login_required
+def home(request):
     return render(request, "core/home.html")
