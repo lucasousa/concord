0a9117571c58c0c3c241bc3c214a54dd588ec3a8
gabrielloliveira <gabrielloliveira097@gmail.com>
2022-01-15 13:21:51 -0300

    lint: black

--- core/tasks.py
+++ core/tasks.py
@@ -1,14 +1,13 @@
-from concord.celery import app
-from core.models import User
-from django.utils import timezone
 from datetime import timedelta
+
+from concord.celery import app
+from django.utils import timezone
+
+from core.models import User
 
 
 @app.task(bind=True)
 def check_status(task_definition):
     users = User.objects.filter(
-        last_ping__lte=timezone.now()-timedelta(minutes=2),
-        status=User.STATUS_ONLINE
+        last_ping__lte=timezone.now() - timedelta(minutes=2), status=User.STATUS_ONLINE
     ).update(status=User.STATUS_OFFLINE)
-     
-    
